0ebca697f3084d52492242604f8b6b69dd95197f
Shubham Karale <58147626+Cyberton207@users.noreply.github.com>
2023-03-24 03:20:29 +0000

    function file

---
+++ index.js
@@ -0,0 +1,11 @@
+function average(a,b,c)
+{
+  return (a+b+c)/3;
+}
+let a = prompt("Enter Value Of a");
+a = Number.parseInt(a);
+let b = prompt("Enter Value Of b");
+a = Number.parseInt(b);
+let c = prompt("Enter Value Of c");
+a = Number.parseInt(c);
+console.log(average(a,b,c));
